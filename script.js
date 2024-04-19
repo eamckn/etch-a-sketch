64afd7df15ccfa17fc44496c9ebe5cd360e24db1
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-04-19 19:31:23 -0400

    Add div eventListener to change background color
    On event 'mouseover', div background color is changed to black.

--- script.js
+++ script.js
@@ -16,8 +16,11 @@ const container = document.querySelector("#container");
 for (let row = 0; row < 16; row++) {
     for (let col = 0; col <16; col++) {
         const newDiv = document.createElement("div");
-        newDiv.textContent = "x"
+        //newDiv.textContent = "x"
+        newDiv.className = "etch";
+        newDiv.addEventListener('mouseover', function () {
+            newDiv.style.backgroundColor = 'black';
+        })
         container.appendChild(newDiv);
     }
 }
-
