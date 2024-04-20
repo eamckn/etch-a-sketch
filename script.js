a1402d670f2b06beaa79647655e16e24bb1dadb6
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-04-20 19:43:45 -0400

    Create outer display for etch-a-sketch

--- script.js
+++ script.js
@@ -12,10 +12,22 @@
 // okay, let's make a row of 16 and give them flex attributes
 
 const container = document.querySelector("#container");
+//const borderWidth = container.style.borderWidth;
+//console.log(borderWidth);
+const containerWidth = container.offsetWidth;
+//console.log(containerWidth)
+const containerHeight = container.offsetHeight;
+//console.log(containerHeight)
 
 for (let row = 0; row < 16; row++) {
-    for (let col = 0; col <16; col++) {
+    for (let col = 0; col < 16; col++) {
         const newDiv = document.createElement("div");
+        const newDivWidth = containerWidth/16;
+        const newDivHeight = containerHeight/16;
+        newDiv.style.width = `${newDivWidth}px`;
+        //console.log(newDiv.style.width);
+        newDiv.style.height = `${newDivHeight}px`;
+        //console.log(newDiv.style.height);
         //newDiv.textContent = "x"
         newDiv.className = "etch";
         newDiv.addEventListener('mouseover', function () {
