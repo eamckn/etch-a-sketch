bdfbad4e98d80938bf5d98e5c870fbc3e67ac122
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-04-22 15:20:04 -0400

    Add button to resize etch-a-sketch grid

--- script.js
+++ script.js
@@ -13,21 +13,40 @@ const containerWidth = container.offsetWidth;
 //console.log(containerWidth)
 const containerHeight = container.offsetHeight;
 //console.log(containerHeight)
+let initialGridSize = 16;
 
-for (let row = 0; row < 16; row++) {
-    for (let col = 0; col < 16; col++) {
-        const newDiv = document.createElement("div");
-        const newDivWidth = containerWidth/16;
-        const newDivHeight = containerHeight/16;
-        newDiv.style.width = `${newDivWidth}px`;
-        //console.log(newDiv.style.width);
-        newDiv.style.height = `${newDivHeight}px`;
-        //console.log(newDiv.style.height);
-        //newDiv.textContent = "x"
-        newDiv.className = "etch";
-        newDiv.addEventListener('mouseover', function () {
-            newDiv.style.backgroundColor = 'black';
-        })
-        container.appendChild(newDiv);
+function buildGrid(gridSize) {
+    for (let row = 0; row < gridSize; row++) {
+        for (let col = 0; col < gridSize; col++) {
+            const newDiv = document.createElement("div");
+            const newDivWidth = containerWidth/gridSize;
+            const newDivHeight = containerHeight/gridSize;
+            newDiv.style.width = `${newDivWidth}px`;
+            //console.log(newDiv.style.width);
+            newDiv.style.height = `${newDivHeight}px`;
+            //console.log(newDiv.style.height);
+            //newDiv.textContent = "x"
+            newDiv.className = "etch";
+            newDiv.addEventListener('mouseover', function () {
+                newDiv.style.backgroundColor = 'black';
+            })
+            container.appendChild(newDiv);
+        }
     }
 }
+
+function removeGrid() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+const resizeButton = document.querySelector("#resize");
+
+resizeButton.addEventListener('click', function () {
+    let gridSize = Number(prompt("Please enter a number desgnating your desired grid width and height"))
+    removeGrid();
+    buildGrid(gridSize);
+})
+
+buildGrid(initialGridSize);
