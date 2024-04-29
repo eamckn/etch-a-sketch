30fae52d3c4af1cd7546a48d3c4c00b357ae9d4b
Eamon McKeon <154269556+eamckn@users.noreply.github.com>
2024-04-29 12:23:49 -0400

    Add code comments and change initialGridSize name

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ const containerHeight = container.offsetHeight;
 
 // Initialize click event listeners for each button
 resizeButton.addEventListener('click', function () {
-    let gridSize = Number(prompt("Please enter a number desgnating your desired grid width and height:"))
+    gridSize = Number(prompt("Please enter a number desgnating your desired grid width and height:"))
     while (gridSize > 99) {
         gridSize = Number(prompt("Grid sizes for 100 and above are not supported. Please enter another number:"))
     }
@@ -40,15 +40,18 @@ opacityButton.addEventListener('click', makeGradient);
 
 
 // Build the starting etch-a-sketch upon page laod
-let initialGridSize = 16;
-buildGrid(initialGridSize);
+let gridSize = 16;
+buildGrid(gridSize);
 
 
-// Functions
+// FUNCTIONS
+
+// Remove all divs, used when resizing the board
 function removeGrid() {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
+    // Set eraser to draw if mouse function prior to resizing was erasing
     if (eraseButton.classList.contains("clicked")) {
         resetEraser();
     }
@@ -59,56 +62,69 @@ function resetEraser() {
     eraseButton.classList.remove("clicked");
 }
 
+// Make squares opaque black
 function drawBlack(event) {
     event.target.style.backgroundColor = 'black';
     event.target.style.opacity = 1;
 }
 
+// Make squares same color as base blank background
 function drawEmpty(event) {
     event.target.style.backgroundColor = BLANK;
     event.target.style.opacity = 1;
 }
- 
+
+// Build grid once prior grid is cleared
 function buildGrid(gridSize) {
     for (let row = 0; row < gridSize; row++) {
         for (let col = 0; col < gridSize; col++) {
 
             const newDiv = document.createElement("div");
+            // Sets width proportionally
             const newDivWidth = containerWidth/gridSize;
-            const newDivHeight = containerHeight/gridSize;
-
             newDiv.style.width = `${newDivWidth}px`;
+            // Sets height proportionally
+            const newDivHeight = containerHeight/gridSize;
             newDiv.style.height = `${newDivHeight}px`;
 
+            // Add class to make DOM selecting easier
             newDiv.className = "etch";
+            // Initialize draw function to be basic opaque black
             newDiv.addEventListener('mouseover', drawBlack)
             container.appendChild(newDiv);
         }
     }
 }
 
+// Change mouseover event on squares to erasing
 function erase() {
 
     let etchList = document.querySelectorAll(".etch");
 
+    // If we aren't yet erasing
     if (eraseButton.classList.contains("clicked") === false) {
         for (let square of etchList) {
             square.removeEventListener('mouseover', drawBlack);
-            square.removeEventListener('mouseover', drawGradient);
             square.addEventListener('mouseover', drawEmpty);
         }
+        // Communicate that we're on the erasing function
         eraseButton.textContent = "Draw";
         eraseButton.classList.add("clicked");
     }
+    // If we're going back to drawing opaque black squares
     else {
         for (let square of etchList) {
             square.removeEventListener('mouseover', drawEmpty);
             square.addEventListener('mouseover', drawBlack);
         }
+        // Tell eraser button to go back to pre-erasing state and function
         resetEraser();
     }
 }
 
+// TURN THIS INTO SOMETHING THAT JUST CLEARS AND REBUILDS WITH CURRENT SIZE
+// THIS WILL RESET EVENT LISTENERS AND MAKE SYNCHRONICITY OF DRAW FUNCTIONS EASIER
+// TO MANAGE/COMBINE
 function clearGrid() {
 
     let etchList = document.querySelectorAll(".etch");
@@ -122,9 +138,14 @@ function clearGrid() {
         }
     }
     resetEraser();
+    if (opacityButton.classList.contains("clicked")) {
+        for (let square of etchList) {
+            square.addEventListener('mouseover', drawGradient(opacity = .1));
+        }
+    }
 }
 
-
+// Make squares a random RGB value
 function randomizeColor() {
 
     let etchList = document.querySelectorAll(".etch");
@@ -139,6 +160,7 @@ function randomizeColor() {
         let randomBlueValue = Math.floor(Math.random() * 255);
 
         square.addEventListener('mouseover', function () {
+            // Prohibit interference with already drawn squares, transparent and opaque
             if (square.style.backgroundColor !== "black") {
                 square.style.backgroundColor = `rgb(${randomRedValue}, ${randomGreenValue}, ${randomBlueValue})`;
             }
@@ -147,7 +169,6 @@ function randomizeColor() {
     resetEraser();
 }
 
-
 function makeGradient() {
 
     let etchList = document.querySelectorAll(".etch");
@@ -155,15 +176,21 @@ function makeGradient() {
     for (let square of etchList) {
         square.removeEventListener('mouseover', drawBlack);
         square.removeEventListener('mouseover', drawEmpty);
+        // Don't reset opacity on already drawn squares, transparent and opaque
         if (square.style.backgroundColor !== "black") {
             square.addEventListener('mouseover', drawGradient(opacity = .1));
         }
     }
+    // Tell eraser button to go back to pre-erasing state and function if it was just used
     if (eraseButton.classList.contains("clicked")) {
         resetEraser();
     }
+    if (!opacityButton.classList.contains("clicked")) {
+        opacityButton.classList.add("clicked");
+    }
 }
 
+// Make squares black with a 10% opacity that increased with each pass of mouseover
 function drawGradient(opacity = .1) {
     return function (event) {
         event.target.style.backgroundColor = "black";
